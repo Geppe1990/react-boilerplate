03ead9937c1e75eb75ccbfe4b0607b45458f1c5a
Giuseppe Vigneri <geppe1990@gmail.com>
2024-06-07 10:20:11 +0200

    Redux

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,32 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '../../store';
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
-interface CounterState {
-	value: number;
+export interface CounterState {
+	value: number
 }
 
 const initialState: CounterState = {
 	value: 0,
-};
+}
 
-const counterSlice = createSlice({
+export const counterSlice = createSlice({
 	name: 'counter',
 	initialState,
 	reducers: {
 		increment: (state) => {
-			state.value += 1;
+			state.value += 1
 		},
 		decrement: (state) => {
-			state.value -= 1;
+			state.value -= 1
+		},
+		incrementByAmount: (state, action: PayloadAction<number>) => {
+			state.value += action.payload
 		},
 		reset: (state) => {
 			state.value = 0;
 		},
 	},
-});
+})
 
-export const { increment, decrement, reset } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, reset } = counterSlice.actions
 
-export const selectCount = (state: RootState) => state.counter.value;
-
-export default counterSlice.reducer;
+export default counterSlice.reducer
